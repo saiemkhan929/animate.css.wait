b5d9db94cf343e67cc58f1a1c3e5436079cd01a0
saiemkhan929 <34841315+saiemkhan929@users.noreply.github.com>
2019-04-21 13:56:11 +0600

    Update animate.css.wait.js

--- animate.css.wait.js
+++ animate.css.wait.js
@@ -3,7 +3,7 @@ jQuery(document).ready(function($){
 //Slow Anchor | Smooth scrolling
 
 // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
+  jQuery("a").on('click', function(event) {
 
     // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
@@ -15,11 +15,11 @@ jQuery(document).ready(function($){
 
       // Using jQuery's animate() method to add smooth page scroll
       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-//       $('html, body').animate({
-//         scrollTop: $(hash).offset().top
-//       }, 800);
+      jQuery("html, body").animate({
+        scrollTop: jQuery(hash).offset().top
+      }, 800);
 	    
-	    alert('');
+	
     } // End if
   });
 
